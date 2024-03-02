d7a0028c73ecb7856b5cf9ca218863b697df8c70
Yahia <yn7599@gmail.com>
2024-03-02 14:33:32 +0200

    auth guard

---
+++ src/shared/guards/auth.guard.ts
@@ -0,0 +1,30 @@
+import { JwtService } from '@nestjs/jwt';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.token;
+    if (!token) {
+      throw new UnauthorizedException('Unauthorized access');
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      request['user_id'] = payload.sub;
+      request['email'] = payload.email;
+    } catch {
+      throw new UnauthorizedException('Unauthorized access');
+    }
+    return true;
+  }
+}
