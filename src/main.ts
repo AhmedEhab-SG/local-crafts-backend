cb89d8e3e874f667e428002a85c9e724b1a27aa7
AhmedEhab-SG <ahmedehab.sg@gmail.com>
2024-03-05 16:46:58 +0200

    added cores() to main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
